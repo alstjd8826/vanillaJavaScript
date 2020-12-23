fe8a6cf6c5cda0281097fe606931b78ae3760bd2
alstjd8826 <53422859+alstjd8826@users.noreply.github.com>
2020-12-23 21:53:11 +0900

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,50 @@
+/*console.log('Im Working. Im JS. Im Beautiful. Im Worth it');
+
+a= 221;
+b=a-5;
+console.log(b);*/
+// 주석
+dsd
+/* 주석
+주석
+주석
+주석
+주석
+주석*/
+/*
+const what = "misnung";// 변수 선언 시 기본은 const
+console.log(what);
+*/
+
+/*
+boolean
+const wat = true;
+*/
+
+/*
+Number
+const wat = 666;
+*/
+
+/*float
+const wat = 55.1;
+console.log(wat);
+*/
+/*
+const msinfo = ["Minsung", "25", "Changwon", true];
+console.log(msinfo);
+*/
+
+//object
+const msinfo = {
+    name:"Minsung",
+    age:25,
+    gender : "male",
+    ishandsome:true,
+    favMovies:["along the gods", "Old boy"],
+    facFood: [
+        {name:"hamburger", fatty : true}, 
+        {name:"noodle", fatty: true}
+    ]
+};
+console.log(msinfo);
